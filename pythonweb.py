6e6836fe49fb7e398fb9c29e02d9d471e5360041
2305a21l39satwik <2305a21l39@sru.edu.in>
2024-11-22 12:50:09 +0530

    Add files via upload

---
+++ pythonweb.py
@@ -0,0 +1,19 @@
+import streamlit as sl ;
+
+sl.title("SRU marks")
+def mark(t,h):
+    return t+h
+    
+tab1,tab2=sl.tabs(["grading imarks","nfo"])
+with tab1:
+    col1,col2,=sl.columns(2)
+    
+    with col1:
+        t=sl. number_input("TELUGU",value=1)
+        h=sl.number_input("hindi",value=1)
+        
+  
+    with col2:
+       sl.write("this is on hold for now")
+       sl.write("this second line")
+       
